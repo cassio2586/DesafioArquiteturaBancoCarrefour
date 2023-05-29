25dd6cb070f07ae9e2d516a954c4017e2457f998
cassio <cassio_renee@hotmail.com>
2023-05-29 12:44:17 -0300

    commit

---
+++ src/Clean.Architecture.Core/Interfaces/ICreateCashService.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Result;
+using Clean.Architecture.Core.CashAggregate.Enums;
+
+namespace Clean.Architecture.Core.Interfaces;
+
+public interface ICreateCashService
+{
+  public Task<Result> Add(string description, decimal amount, TransactionTypeEnum transactionTyoe);
+}
